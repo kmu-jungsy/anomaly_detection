16dfea02dfec3f52919960cd981733a8c209ba9c
kmu-jungsy <135489568+kmu-jungsy@users.noreply.github.com>
2026-05-17 02:22:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 Invoke-WebRequest `
   -Uri "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz" `
   -OutFile "ffmpeg-release-amd64-static.tar.xz"
+
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
