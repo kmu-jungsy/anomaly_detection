c6ca2e397b4e1efff4bade9c9b8edfdb1092becf
kmu-jungsy <135489568+kmu-jungsy@users.noreply.github.com>
2026-05-22 09:04:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-Invoke-WebRequest `
-  -Uri "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz" `
-  -OutFile "ffmpeg-release-amd64-static.tar.xz"
+import cv2
+import numpy as np
 
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+mask_path = "mask/02_mask.jpg"
+mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+print("shape:", mask.shape)
+print("min:", mask.min())
+print("max:", mask.max())
+print("unique values:", np.unique(mask)[:50])
+print("num unique:", len(np.unique(mask)))
