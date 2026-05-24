ce7ae002d084ed84ea6d19aff99c455292af2e0c
kmu-jungsy <135489568+kmu-jungsy@users.noreply.github.com>
2026-05-24 21:09:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-for d in data/posco/train/*; do
-    if [ -d "$d" ]; then
-        echo "$d: $(find "$d" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | wc -l)"
-    fi
-done
+find data/posco/train -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | wc -l
